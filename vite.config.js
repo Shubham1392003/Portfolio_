343f5eb2b392557d5aca211947c8fa567d7013d5
Shubham Kendre <skendre380@gmail.com>
2025-09-27 19:11:14 +0530

    new text effect and about section is created

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,10 @@ export default defineConfig({
     tailwindcss(),
     react(),
   ],
+  resolve: {
+    alias: {
+      // Maps "@/" alias to the "src" directory (D:/Portfolio/Portfolio_/src)
+      "@/": path.resolve(__dirname, "./src/"),
+    },
+  },
 })
